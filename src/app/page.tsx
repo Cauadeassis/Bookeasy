43284e3449f5488eacbca7fe0c568a9287b48895
Cauã <cauadeassis2009@gmail.com>
2026-03-22 11:38:12 +0000

    Initial design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,13 @@
 
 import { useState } from "react";
 import styles from "./styles.module.css";
-import EmailField from "./components/emailField";
-import PasswordField from "./components/passwordField";
-import StepLink from "./components/stepLink";
-import { createAccount } from "./services/authentication";
-import { sendEmail } from "./services/email";
-
-type Step =
-  | "createAccount"
-  | "login"
-  | "forgotPassword"
-  | "verifyingCode"
-  | "newPassword"
-  | "success";
+import { createAccount } from "../infra/services/authentication";
+import { sendEmail } from "../infra/services/email";
 
 export default function Login() {
-  const [step, setStep] = useState<Step>("createAccount");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [aberto, setAberto] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   async function handleCreateAccount() {
@@ -32,116 +21,62 @@ export default function Login() {
       emailType: "forgot-password",
     });
   }
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div className={styles.body}>
-      <div className={styles.siteContent}>
-        <header></header>
-
-        <main>
-          <form>
-            {step === "createAccount" && (
-              <>
-                <EmailField value={email} onChange={setEmail} />
-                <PasswordField value={password} onChange={setPassword} />
-                <StepLink onClick={() => setStep("login")}>
-                  Já tenho uma conta
-                </StepLink>
-
-                <button
-                  type="button"
-                  className={styles.mainButton}
-                  onClick={handleCreateAccount}
-                >
-                  Criar
-                </button>
-              </>
-            )}
-            {step === "login" && (
-              <>
-                <EmailField value={email} onChange={setEmail} />
-
-                <PasswordField value={password} onChange={setPassword} />
-                <div className={styles.buttonFlexContainer}>
-                  <StepLink onClick={() => setStep("forgotPassword")}>
-                    Esqueci a senha
-                  </StepLink>
-                  <StepLink onClick={() => setStep("createAccount")}>
-                    Já tenho uma conta
-                  </StepLink>
-                </div>
-
-                <button type="button" className={styles.mainButton}>
-                  Entrar
-                </button>
-              </>
-            )}
-
-            {step === "forgotPassword" && (
-              <>
-                <EmailField value={email} onChange={setEmail} />
-
-                <button
-                  type="button"
-                  onClick={handleSendCode}
-                  className={styles.mainButton}
-                >
-                  Enviar código
-                </button>
-              </>
-            )}
-
-            {step === "verifyingCode" && (
-              <>
-                <label>
-                  Código de verificação
-                  <input
-                    type="text"
-                    inputMode="numeric"
-                    maxLength={5}
-                    placeholder="12345"
-                    required
-                  />
-                </label>
-
-                <button
-                  type="button"
-                  onClick={() => setStep("newPassword")}
-                  className={styles.mainButton}
-                >
-                  Verificar código
-                </button>
-              </>
-            )}
-
-            {step === "newPassword" && (
-              <>
-                <PasswordField value={password} onChange={setPassword} />
-
-                <button
-                  type="button"
-                  onClick={() => setStep("success")}
-                  className={styles.mainButton}
-                >
-                  Criar nova senha
-                </button>
-              </>
-            )}
-
-            {step === "success" && (
-              <>
-                <p>Senha redefinida com sucesso 🎉</p>
-
-                <button
-                  type="button"
-                  onClick={() => setStep("login")}
-                  className={styles.mainButton}
-                >
-                  Voltar para login
-                </button>
-              </>
-            )}
-          </form>
+      <div className={styles.book}>
+        <header
+          className={`${styles.bookCover} ${aberto ? styles.openedCover : ""}`}
+          onClick={() => setAberto(!aberto)}
+        >
+          <div className={styles.coverFront}>
+            {/* conteúdo que aparece quando fechado*/}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#463131"
+              viewBox="0 0 24 24"
+            >
+              {/* Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free */}
+              <path d="M21 3h-7c-.77 0-1.47.3-2 .78-.53-.48-1.23-.78-2-.78H3c-.55 0-1 .45-1 1v15c0 .55.45 1 1 1h5.76c.53 0 1.04.21 1.41.59l1.12 1.12s.02.01.03.02c.09.08.18.15.29.2.12.05.25.08.38.08s.26-.03.38-.08c.11-.05.21-.12.29-.2 0 0 .02-.01.03-.02l1.12-1.12c.37-.37.89-.59 1.41-.59h5.76c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1ZM8.76 18H4V5h6c.55 0 1 .45 1 1v12.69c-.66-.44-1.44-.69-2.24-.69M20 18h-4.76c-.8 0-1.58.25-2.24.69V6c0-.55.45-1 1-1h6z"></path>
+            </svg>
+            <h1>Bookeasy</h1>
+          </div>
+          <div className={styles.coverBack}>
+            {/* conteúdo que aparece quando aberto*/}
+          </div>
+        </header>
+        <main
+          className={`${styles.bookContent} ${aberto ? styles.openedContent : ""}`}
+        >
+          <div className={styles.fieldsContainer}>
+            <div className={styles.field}>
+              <label>Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={handleChangeEmail}
+                placeholder="Digite o email"
+                required
+              />
+            </div>
+            <div className={styles.field}>
+              <label>Senha</label>
+              <input
+                type="password"
+                value={password}
+                onChange={handleChangePassword}
+                placeholder="Digite a senha"
+                required
+              />
+            </div>
+          </div>
+          <button onClick={handleCreateAccount}>Cadastrar</button>
         </main>
       </div>
     </div>
